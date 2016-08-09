0bc1c310fe829c9a94b1861958bafd27756e39ee
ramnivasindani <ram.0737@gmail.com>
2016-08-09 04:52:26 -0700

    Updated as per the course instructions, added listview, adapter and binding between fake weather data using ArrayAdapter and ArrayList

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -57,6 +61,18 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            ArrayList<String> forecastEnty = new ArrayList<>();
+            forecastEnty.add("Today - Sunny - 88/63");
+            forecastEnty.add("Tomorrow - Foggy - 70/46");
+            forecastEnty.add("Weds - Cloudy - 72/63");
+            forecastEnty.add("Thurs - Rainy - 64/51");
+            forecastEnty.add("Fri - Foggy - 70/46");
+            forecastEnty.add("Sat - Sunny - 76/68");
+
+            ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, forecastEnty);
+
+            ListView listViewForecast = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listViewForecast.setAdapter(forecastAdapter);
             return rootView;
         }
     }
